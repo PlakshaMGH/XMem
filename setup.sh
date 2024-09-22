8a4d3beb04e859d14e526a581b118f085c01ec5d
abdksyed <abdksyed@gmail.com>
2024-09-22 17:24:26 +0530

    setting up new vm instance

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install ffmpeg libsm6 libxext6  -y
+sudo apt install -y unzip
+sudo apt-get install -y git-lfs
+
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
+
+rclone config 
+
+rclone copy remote:endovis/endo17/data/frames /workspace/data/frames --progress --transfers 32
+rclone copy remote:endovis/endo17/data/masks /workspace/data/masks --progress --transfers 32
+
+git clone https://github.com/PlakshaMGH/XMem.git
+
+cd workspace/XMem
+pip install -r requirements.txt
+pip install -e .
+
